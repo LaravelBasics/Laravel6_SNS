4fd32340a2d49bd4a326dedbaf33698bf523deba
abcdefghijkok <aaa111sssbbbb@gmail.com>
2024-10-25 14:17:35 +0900

    update:デプロイ時はmixjs→vuejsをCDNに変更

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  
-
-
 
   <title>@yield('title')</title>
   <!-- Font Awesome -->
@@ -29,7 +26,10 @@
 
   
   <!-- Vue.js -->
-  <script src="{{ mix('js/app.js') }}"></script>
+  <!-- <script src="{{ mix('js/app.js') }}"></script> -->
+
+  <!-- Vue 3 CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/vue@3.3.0/dist/vue.global.js"></script>
 
   <!-- JQuery -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
