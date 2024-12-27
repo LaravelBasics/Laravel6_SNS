523076341af50a195d4c7d121608a87e77de60a8
abcdefghijkok <aaa111sssbbbb@gmail.com>
2024-12-27 11:58:59 +0900

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,9 +13,56 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
   <!-- Material Design Bootstrap -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+
+  <style>
+        /* ローディング画面 */
+        #loading-screen {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            z-index: 9999;
+        }
+
+        /* リングのスタイル */
+        .spinner {
+            border: 8px solid #f3f3f3;
+            /* 薄い色 */
+            border-top: 8px solid #3498db;
+            /* トップ部分の色 */
+            border-radius: 50%;
+            /* 円形にする */
+            width: 50px;
+            /* リングのサイズ */
+            height: 50px;
+            animation: spin 1s linear infinite;
+            /* アニメーションの設定 */
+        }
+
+        /* 回転のアニメーション */
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
+    @yield('style')
 </head>
 
 <body>
+<div id="loading-screen">
+  <div class="spinner"></div> <!-- 回転するリング -->
+</div>
+
 <div id="app">
     @include('nav') <!-- ナビゲーションバーの読み込み -->
     
@@ -23,7 +70,15 @@
         @yield('content')
     </main>
 </div>
-
+<script>
+        // ページが読み込まれたらローディング画面を非表示にする 
+        window.onload = function() {
+            const loadingScreen = document.getElementById('loading-screen');
+            loadingScreen.style.display = 'none';
+        };
+</script>
+  <!-- スクリプト用のセクション -->
+  @yield('scripts')
   
   <!-- Vue.js -->
   <script src="{{ mix('js/app.js') }}"></script>
